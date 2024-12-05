198a3c818ba4f94dd9f15b16bb10f8eaebd494dc
JuanJoseRestrepoArango <restrepoj811@gmail.com>
2024-12-05 20:38:00 +0100

    IntroJs/MedQ

---
+++ responsive/js/respo.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    console.log("El archivo JavaScript ha sido cargado");
+    const header = document.querySelector("header");
+    const boton = document.querySelector("button");
+    // boton.onclick = function(){
+    //     header.innerHTML = "<h1>" + (parseInt(header.innerText)+1)+" "+ "</h1>" ;
+    //     // header.innerHTML = "<h1>${parseInt(header.innerText)+1}</h1>" ;
+    // }
+    // header.innerHTML = "<h1>2</h1>"
+
+    boton.addEventListener("click",function(){
+        header.innerHTML = "<h1>" + (parseInt(header.innerText)+1)+" "+ "</h1>" ;
+    })
+}
