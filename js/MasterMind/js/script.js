e64fc3be71779bdcc8784a524fd78dbf7c3f7b9b
JuanJoseRestrepoArango <restrepoj811@gmail.com>
2025-03-21 16:58:33 +0100

    MasterMind

--- js/MasterMind/js/script.js
+++ js/MasterMind/js/script.js
@@ -1,35 +1,73 @@
 (function(){
-    let cont ;
+    let cont = 0;
     contenedor = document.querySelector('.contenedorCampos');
-    for (let i = 0; i< 4;i++){
-        cont = i+1;
+    const opciones = ['V','A','C','N','A','R'];
+    const respuesta = [];
+  
+    for (let i = 0; i< 8;i++){
+
         let div = document.createElement('DIV')
         div.classList.add('contenedorLinea')
-        for(let j = 0;j<8;j++){
-            casilla = document.createElement('DIV');
+        div.id = `cl${i+1}`;
+        let resultados = document.createElement('DIV')
+        resultados.classList.add('resultados')
+        for(let j = 0;j<4;j++){
+            cont = cont+1;
+            let casilla = document.createElement('DIV');
             casilla.classList.add('casilla');
             casilla.id = cont;
-            cont = cont + 4;
             casilla.innerHTML = '&nbsp';
             div.appendChild(casilla);
+            let contenedorL = document.createElement('DIV');
+            contenedorL.classList.add('contenedorLinea');
+            let resultado = document.createElement('DIV');
+            resultado.classList.add('resultado');
+            resultado.id = `res${cont}`;
+            resultado.innerHTML = '&nbsp';
+            contenedorL.appendChild(resultado);
+            resultados.appendChild(contenedorL);
         }
+        div.appendChild(resultados);
         contenedor.appendChild(div);
-        console.log('hola')
+       
     }
-    cont = cont-4;
-    console.log(cont);
-    document.addEventListener('click', function(e){
-        
-        if(e.target.id != ''){
-            let color = document.getElementById(`${cont}`);
-            console.log(color)
-            color.classList.add(`${e.target.id}`);
-            cont--;
-        }else{
+    generarRespuesta();
+    cont = 32;
+    
+    colores = document.querySelector('.colores');
+    colores.addEventListener('click', function(e){
+        if(e.target.id == 'error' || e.target.id == 'basura' || e.target.id == 'basurera'){
+            
+
             let color = document.getElementById(cont+1);
             color.classList = null
             color.classList.add('casilla')
             cont++;
+        }else{
+            let color = document.getElementById(`${cont}`);
+            color.classList.add(`${e.target.id}`);
+            cont--;
         }
+        if(cont < 0){
+            alert("Juego Terminado")
+            window.location.reload();
+        }
+        console.log(cont)
     })
+
+
+    
+
+    function alea(ls,li){
+        return Math.floor(Math.random() * (ls - li + 1)) + li;
+    }
+
+    function generarRespuesta(){
+        for(let i = 0;i<4;i++){
+            respuesta.push(opciones[alea(0,opciones.length)]);
+
+        }
+        console.log(respuesta);
+    }
+
 })();
